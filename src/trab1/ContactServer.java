ad6438645ca02037bd6faa0c9012dbf4a701de69
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2014-04-04 10:59:50 +0100

    ContactServer improved.

--- src/trab1/ContactServer.java
+++ src/trab1/ContactServer.java
@@ -1,25 +1,25 @@
 package trab1;
 
 import java.net.InetAddress;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.*;
 import java.util.*;
 
-public class ContactServer {
+public class ContactServer extends UnicastRemoteObject {
 	
-	static Map<String, InetAddress> serversListIP;
-	static Map<String, ArrayList<String>> serversListUsers;
+	private static final long serialVersionUID = 1L;
 	
-	public static void main( String[] args) throws Exception
-	{
-		if( args.length != 0) {
-			System.out.println("Use: java trab1.ContactServer");
-			return;
-		}
-		
+	private Map<String, InetAddress> serversListIP;
+	private Map<String, ArrayList<String>> serversListUsers;
+	
+	protected ContactServer() throws RemoteException {
+		super();
 		serversListIP = new HashMap<String, InetAddress>();
-		serversListUsers = new HashMap<String, ArrayList<String>>();		
-	}
+		serversListUsers = new HashMap<String, ArrayList<String>>();
+	}	
 
-	public static void registerServer(String serverName, InetAddress serverIP, String userName)
+	public void registerServer(String serverName, InetAddress serverIP, String userName)
 	{
 		serversListIP.put(serverName, serverIP);
 		ArrayList<String> users = new ArrayList<String>();
@@ -31,11 +31,36 @@ public class ContactServer {
 	 * Lista servidores acessiveis a determinado utilizador
 	 * @return
 	 */
-	protected String[] listServers(String userName)
+	public String[] listServers(String userName)
 	{
 		String temp[] = new String[serversListIP.size()];
 		temp = (String[]) serversListIP.keySet().toArray();
 		return temp;
+	}
+	
+	public static void main( String[] args) throws Exception
+	{
+		if( args.length != 0) {
+			System.out.println("Use: java trab1.ContactServer");
+			return;
+		}
+		
+		System.getProperties().put( "java.security.policy", "trab1/policy.all");
+		
+		if( System.getSecurityManager() == null) {
+			System.setSecurityManager( new RMISecurityManager());
+		}
+
+		try { // start rmiregistry
+			LocateRegistry.createRegistry( 1099);
+		} catch( RemoteException e) { 
+			// if not start it
+			// do nothing - already started with rmiregistry
+		}
+		
+		ContactServer server = new ContactServer();
+		Naming.rebind( "/trabalhoSD", server); ---- esta a dar erro. acesso negado. policy?
+		System.out.println( "ContactServer running...");
 	}
 	
 }
